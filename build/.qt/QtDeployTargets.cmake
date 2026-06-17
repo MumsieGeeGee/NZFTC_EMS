f25f089bcb8579a2f678a53a4a8b7995618f8723
Georgia <osullivangeorgia@gmail.com>
2026-06-17 22:03:51 +1200

    QT UI Intial Setup and Test Build

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_NZFTC_EMS_UI_FILE D:/DiplomaSoftwareDevelopment/.IntegrateStudios2/Proposal Assessment/NZFTC_EMS/build/NZFTC_EMS_UI.exe)
+set(__QT_DEPLOY_TARGET_NZFTC_EMS_UI_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_NZFTC_EMS_UI_RUNTIME_DLLS C:/Qt/6.9.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.2/msvc2022_64/bin/Qt6Cored.dll)
---
+++ main/UI/src/build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_NZFTC_EMS_UI_FILE D:/DiplomaSoftwareDevelopment/.IntegrateStudios2/Proposal Assessment/NZFTC_EMS/main/UI/src/build/RelWithDebInfo/NZFTC_EMS_UI.exe)
+set(__QT_DEPLOY_TARGET_NZFTC_EMS_UI_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_NZFTC_EMS_UI_RUNTIME_DLLS C:/Qt/6.9.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/msvc2022_64/bin/Qt6Core.dll)
